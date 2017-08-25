cf2e6e2d19f6b049bcdb452898dfaa4d6f831978
fanyang <1050549505@qq.com>
2017-08-25 15:08:22 +0800

    Initial Commit

---
+++ FYVideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYVideoPlayerDemo
+//
+//  Created by 樊杨 on 2017/8/25.
+//  Copyright © 2017年 YHCRT.FY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYVideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYVideoPlayerDemo
+//
+//  Created by 樊杨 on 2017/8/25.
+//  Copyright © 2017年 YHCRT.FY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
